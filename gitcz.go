ded2a32ce63f5bcbc9dc25bd2f2551584fbaea97
xiaoqidun <xiaoqidun@gmail.com>
2025-11-27 16:35:26 +0800

    feat(构建迁移): 构建迁移

--- gitcz.go
+++ gitcz.go
@@ -32,8 +32,8 @@ var (
 	InputScopePrompt          = "说明本次提交的影响范围(必填): "
 	InputSubjectPrompt        = "对本次提交进行简短描述(必填): "
 	InputBodyPrompt           = "对本次提交进行完整描述(选填): "
-	InputBreakingChangePrompt = "如果当前代码版本与上一版本不兼容,对变动、变动的理由及迁移的方法进行描述(选填): "
-	InputClosesPrompt         = "如果本次提交针对某个issue,列出关闭的issues(选填): "
+	InputBreakingChangePrompt = "如果当前代码版本与上一版本不兼容, 对变动、变动的理由及迁移的方法进行描述(选填): "
+	InputClosesPrompt         = "如果本次提交针对某个issue, 列出关闭的issues(选填): "
 )
 
 var CzTypeList = []CzType{
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/xiaoqidun/gitcz
 
-go 1.21.2
+go 1.21.0
